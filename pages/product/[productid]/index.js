053ed285ad95d6315e1eede944997d963dfeafc7
Viraj Jadhav <viraj28j@gmail.com>
2021-10-28 22:13:01 +0530

    nested routes tut8

---
+++ pages/product/[productid]/index.js
@@ -0,0 +1,11 @@
+import {useRouter} from 'next/router'
+
+function ProductDetails () {
+    const router = useRouter()
+    const {productid} = router.query
+    return <h1>Details about product {productid}</h1>
+    //Nested Dynamic Routing Example
+    // id for query can be given number or some word eg.greekGod
+}
+
+export default ProductDetails;
