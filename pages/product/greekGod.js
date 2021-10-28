6db27c778b24b8eb272b3622c34f4f1c55acad23
Viraj Jadhav <viraj28j@gmail.com>
2021-10-28 22:05:25 +0530

    dynamic routes tut7 helpful in the list detail pattern n any ui app

---
+++ pages/product/greekGod.js
@@ -0,0 +1,5 @@
+function listmewithRoute () {
+    return <h1>Nervebody GreekGod on his way 🤩!!</h1>
+}
+
+export default listmewithRoute
---
+++ pages/product/index.js
@@ -0,0 +1,11 @@
+function Products () {
+    return (
+        <>
+          <h2>Product 😉</h2>
+          <h2>Product 😇</h2>
+          <h2>Product 😁 </h2>
+        </>
+    )
+}
+
+export default Products
